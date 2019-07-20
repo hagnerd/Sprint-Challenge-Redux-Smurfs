d1c346a42dc721c95aa8fd9e0d72440be636509c
Matt Hagner <web@matthagner.com>
2019-07-20 12:30:59 -0500

    Add ability to create smurf
    The user can now create a smurf, and will be redirected to the home page
    after they've done so

---
+++ smurfs/src/components/smurf-form.js
@@ -0,0 +1,84 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class SmurfForm extends React.Component {
+  state = {
+    name: this.props.name,
+    age: this.props.age,
+    height: this.props.height
+  };
+
+  static propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    height: PropTypes.string,
+    handleSubmit: PropTypes.func.isRequired,
+    submitText: PropTypes.string,
+    submitting: PropTypes.bool.isRequired,
+    error: PropTypes.string
+  };
+
+  static defaultProps = {
+    name: "",
+    age: 0,
+    height: "",
+    submitText: "Create a Smurf"
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.handleSubmit(this.state);
+  };
+
+  render() {
+    const { name, age, height } = this.state;
+
+    return (
+      <>
+        {this.props.error && <p style={{ color: "red" }}>{this.props.error}</p>}
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="age">Age:</label>
+          <input
+            id="age"
+            name="age"
+            type="number"
+            min="0"
+            value={age}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="height">Height:</label>
+          <input
+            id="height"
+            name="height"
+            type="text"
+            value={height}
+            onChange={this.handleChange}
+          />
+          <br />
+          {this.props.submitting ? (
+            <p>Submitting Smurf...</p>
+          ) : (
+            <button type="submit">{this.props.submitText}</button>
+          )}
+        </form>
+      </>
+    );
+  }
+}
