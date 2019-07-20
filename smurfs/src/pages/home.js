bc2eddf095e6e82448468f82cfdd9971a89c8c1c
Matt Hagner <web@matthagner.com>
2019-07-20 11:56:56 -0500

    Complete requirements for rendering smurfs
    Create a route for the smurfs to render at, and set up the fetching and
    rendering of smurfs.

---
+++ smurfs/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SmurfsList from "../components/smurfs-list";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Welcome to the Smurf Village</h1>
+
+      <SmurfsList />
+    </>
+  );
+}
