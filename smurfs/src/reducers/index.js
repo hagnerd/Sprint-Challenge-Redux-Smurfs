193e29113ff30f5673f3a9cc99a8140fa74c2146
Matt Hagner <web@matthagner.com>
2019-07-20 11:27:40 -0500

    Implement initialState and rootReducer
    Set up basic reducer and initial state for fetching smurfs.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,8 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE
+} from "../actions";
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -14,10 +16,37 @@
  }
 */
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  error: null
+};
+
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SMURFS_START: {
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    }
+
+    case FETCH_SMURFS_SUCCESS: {
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: null,
+        smurfs: action.payload
+      };
+    }
+    case FETCH_SMURFS_FAILURE: {
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+    }
+    default:
+      return state;
+  }
+}
